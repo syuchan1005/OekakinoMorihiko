7e4c25d8abce2fee865c0c6db332a1415935373a
syuchan1005@syu_chan_1005 <syuchan.dev@gmail.com>
2016-10-14 23:41:19 +0900

    Javascriptを小分け

---
+++ src/main/resources/public/scripts/chat.js
@@ -0,0 +1,52 @@
+/**
+ * Created by syuchan on 2016/10/14.
+ */
+
+
+var chatUserText = document.getElementById("username");
+var chatText = document.getElementById("chattext");
+document.getElementById("chatsend").addEventListener("click", sendChat, false);
+
+document.onkeydown = function (e) {
+    if (typeof e.modifiers == 'undefined' ? e.ctrlKey : e.modifiers & Event.CONTROL_MASK) {
+        if (e.which == 13) { //enter
+            sendChat();
+        }
+    }
+}
+
+var chat_list = document.getElementById("chatcontentlist");
+
+function appendChat(text, self) {
+    var ele = document.createElement("article");
+    ele.id = self ? "mychatcontent" : "chatcontent";
+    ele.innerHTML = lineWrap(text, 21);
+    chat_list.insertBefore(ele, chat_list.firstChild);
+}
+
+function lineWrap(text, maxlength) {
+    var resultText = [""];
+    var len = text.length;
+    if (maxlength >= len) {
+        return text;
+    }
+    else {
+        var totalStrCount = parseInt(len / maxlength);
+        if (len % maxlength != 0) {
+            totalStrCount++
+        }
+
+        for (var i = 0; i < totalStrCount; i++) {
+            if (i == totalStrCount - 1) {
+                resultText.push(text);
+            }
+            else {
+                var strPiece = text.substring(0, maxlength - 1);
+                resultText.push(strPiece);
+                resultText.push("<br>");
+                text = text.substring(maxlength - 1, text.length);
+            }
+        }
+    }
+    return resultText.join("");
+}
---
+++ src/main/resources/public/scripts/websocket.js
@@ -0,0 +1,70 @@
+/**
+ * Created by syuchan on 2016/10/14.
+ */
+var keepAlive;
+var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/web/");
+
+webSocket.onopen = function (event) {
+    keepAlive = setInterval(function () {
+        webSocket.send("Keep-Alive");
+    }, 150000);
+};
+
+webSocket.onclose = function (event) {
+    clearInterval(keepAlive);
+    alert("WebSocket connection closed")
+};
+
+var count = document.getElementById("sessioncount");
+webSocket.onmessage = function (event) {
+    var json = JSON.parse(event.data);
+    if (json.session_count == undefined) {
+        count.innerHTML = "接続人数" + json.session_count_load + "人";
+    } else {
+        count.innerHTML = "接続人数" + json.session_count + "人";
+        switch (json.mode) {
+            case "paint":
+                if (json.size == "AllClear") {
+                    canvasClear();
+                } else {
+                    draw(json.size, json.color, json.alpha, json.x, json.y);
+                }
+                break;
+            case "chat":
+                appendChat(json.text, false);
+                break;
+            case "fill":
+                ctx.globalAlpha = json.alpha;
+                ctx.fillStyle = json.color;
+                ctx.fillFlood(json.x, json.y, 40);
+                break;
+        }
+    }
+};
+
+function sendDraw(Mode, Size, Color, Alpha, X, Y) {
+    var json = new Object();
+    json.mode = Mode;
+    json.size = Size;
+    json.color = Color;
+    json.alpha = Alpha;
+    json.x = X;
+    json.y = Y;
+    webSocket.send(JSON.stringify(json));
+}
+
+function sendChat() {
+    var o = new Object();
+    o.mode = "chat";
+    var user = chatUserText.value;
+    var text = chatText.value;
+    if (text == undefined || text == "") return;
+    o.text = (user == undefined || user == "" ? "匿名" : user) + "<br>" + text.replace("<", "&lt;").replace(">", "&gt;");
+    webSocket.send(JSON.stringify(o));
+    appendChat(o.text, true);
+    chatText.value = "";
+}
+
+function sendWebSocket(obj) {
+    webSocket.send(JSON.stringify(obj));
+}
